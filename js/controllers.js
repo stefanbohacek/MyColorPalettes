f41ec184bb592d7aa60803bb684790a2997da579
Stefan Bohacek <stefan@fourtonfish.com>
2013-09-28 14:27:49 +0200

    First release

---
+++ js/controllers.js
@@ -0,0 +1,132 @@
+function ColorListCtrl($scope) {
+$scope.colors = [
+	{
+	"title": "1943",
+	"data": [{
+		"name": "TROPICAL SMOOTHIE",
+		"code": "#c5506d"
+		},
+		{
+		"name": "HRON GREEN",
+		"code": "#859672"
+		},
+		{
+		"name": "EASTERN SPICE",
+		"code": "#d7a77f"
+		},
+		{
+		"name": "MAJESTIC MOUNT",
+		"code": "#7e7f93"
+		}
+	],
+	"tags": [
+		"vintage"
+	]
+	},
+	{
+		"title": "1958",
+		"data": [{
+			"name": "BLACK LACQUER",
+			"code": "#3e3e3e"
+			},
+			{
+			"name": "DAREDEVIL",
+			"code": "#ab3e41"
+			},
+			{
+			"name": "WHITENED SAGE",
+			"code": "#dce0d2"
+			},
+			{
+			"name": "DARK TRUFFLE",
+			"code": "#574a44"
+			}
+		],
+	"tags": [
+		"vintage",
+		"favorite",
+		"dark"
+	]
+	},
+	{
+		"title": "Watermelon",
+		"data": [{
+			"name": "#7D8A2E",
+			"code": "#7D8A2E"
+			},
+			{
+			"name": "#C9D787",
+			"code": "#C9D787"
+			},
+			{
+			"name": "#FFFFFF",
+			"code": "#FFFFFF"
+			},
+			{
+			"name": "#FFC0A9",
+			"code": "#FFC0A9"
+			},
+			{
+			"name": "#FF8598",
+			"code": "#FF8598"
+			}
+		],
+	"tags": [
+		"kuler",
+		"summer",
+		"bright"
+	]
+	},
+	{
+		"title": "Thought Provoking",
+		"data": [{
+			"name": "THOUGHT BY SOME",
+			"code": "#ecd078"
+			},
+			{
+			"name": "THOUGHTFUL BRICK",
+			"code": "#d95b43"
+			},
+			{
+			"name": "THOUGHTLESS",
+			"code": "#c02942"
+			},
+			{
+			"name": "THOUGHT YOU WERE",
+			"code": "#542437"
+			},
+			{
+			"name": "THOUGHTLESS",
+			"code": "#53777a"
+			}			
+		],
+	"tags": [
+		"colourlovers",
+		"favorite"
+	]
+	},
+	{
+		"title": "1987",
+		"data": [{
+			"name": "LIME POP",
+			"code": "#c4cb23"
+			},
+			{
+			"name": "APPLETINI",
+			"code": "#8b9532"
+			},
+			{
+			"name": "JET BLACK",
+			"code": "#2d2e30"
+			},
+			{
+			"name": "STAR SHINE",
+			"code": "#f6f7e7"
+			}
+		],
+	"tags": [
+		"vintage"
+	]
+	}
+];
+}
